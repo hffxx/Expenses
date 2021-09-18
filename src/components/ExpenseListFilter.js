b10476eaab09b285a350c1e76ab9c62ccfd3f45a
Hffxx <hoffix0123@gmail.com>
2021-09-18 17:16:09 +0200

    added moment, fixed scroll bug

--- src/components/ExpenseListFilter.js
+++ src/components/ExpenseListFilter.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   TextField,
   InputLabel,
@@ -6,7 +6,7 @@ import {
   Select,
   MenuItem,
 } from "@material-ui/core";
-import { connect, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { setTextFilter } from "../redux/actions/filterActions/setTextFilter";
 import { sortByAmount } from "../redux/actions/filterActions/sortByAmount";
 import { sortByDate } from "../redux/actions/filterActions/sortByDate";
@@ -15,47 +15,57 @@ import DatePicker from "@mui/lab/DatePicker";
 import LocalizationProvider from "@mui/lab/LocalizationProvider";
 import DateAdapter from "@mui/lab/AdapterMoment";
 import { useDispatch } from "react-redux";
+import moment from "moment";
 
 function ExpenseListFilter() {
   const dispatch = useDispatch();
   const filters = useSelector((state) => state.filters);
   const classes = useStyles();
-  const [startDate, setStartDate] = React.useState(new Date());
+  const [date, setDate] = useState({
+    startDate: moment(),
+    endDate: moment(),
+  });
   return (
     <div>
-      <LocalizationProvider dateAdapter={DateAdapter}>
-        <TextField
-          id="outlined-basic"
-          label="Filter"
-          variant="outlined"
-          onChange={(e) => dispatch(setTextFilter(e.target.value))}
-        ></TextField>
-        <FormControl variant="outlined" className={classes.formControl}>
-          <InputLabel id="demo-simple-select-outlined-label">
-            Sort by
-          </InputLabel>
-          <Select
-            labelId="demo-simple-select-outlined-label"
-            id="demo-simple-select-outlined"
-            label="SortBy"
-            value={filters.sortBy}
-            onChange={(e) => {
-              if (e.target.value === "date") {
-                dispatch(sortByDate(e.target.value));
-              } else if (e.target.value === "amount") {
-                dispatch(sortByAmount(e.target.value));
-              }
-            }}
-          >
-            <MenuItem value="date">Date</MenuItem>
-            <MenuItem value="amount">Amount</MenuItem>
-          </Select>
-        </FormControl>
-        <DatePicker
-          label="Basic example"
-          value={startDate}
+      <TextField
+        id="outlined-basic"
+        label="Filter"
+        variant="outlined"
+        onChange={(e) => dispatch(setTextFilter(e.target.value))}
+      ></TextField>
+      <FormControl variant="outlined" className={classes.formControl}>
+        <InputLabel id="demo-simple-select-outlined-label">Sort by</InputLabel>
+        <Select
+          labelId="demo-simple-select-outlined-label"
+          id="demo-simple-select-outlined"
+          label="SortBy"
+          value={filters.sortBy}
           onChange={(e) => {
-            setStartDate(e);
+            if (e.target.value === "date") {
+              dispatch(sortByDate(e.target.value));
+            } else if (e.target.value === "amount") {
+              dispatch(sortByAmount(e.target.value));
+            }
+          }}
+        >
+          <MenuItem value="date">Date</MenuItem>
+          <MenuItem value="amount">Amount</MenuItem>
+        </Select>
+      </FormControl>
+      <LocalizationProvider dateAdapter={DateAdapter}>
+        <DatePicker
+          label="Start Date"
+          value={date.startDate}
+          onChange={(newStartDate) => {
+            setDate({ ...date, startDate: newStartDate });
+          }}
+          renderInput={(params) => <TextField {...params} />}
+        />
+        <DatePicker
+          label="end Date"
+          value={date.endDate}
+          onChange={(newEndDate) => {
+            setDate({ ...date, endDate: newEndDate });
           }}
           renderInput={(params) => <TextField {...params} />}
         />
