105345f16b87f8496100ce103536987a1bd8e5a1
Maciej <hoffix0123@gmail.com>
2021-10-06 18:16:39 +0200

    migration to material ui v5

--- src/App.js
+++ src/App.js
@@ -1,35 +1,55 @@
 import "./App.css";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { AppBar, Typography, Container } from "@material-ui/core";
-import useStyles from "./styles";
-
+import {
+  ThemeProvider,
+  createTheme,
+  makeStyles,
+} from "@material-ui/core/styles";
 import Navbar from "./components/Navbar/Navbar";
 import LoginPage from "./components/LoginPage";
 import ExpenseDashboardPage from "./components/ExpenseDashboard";
 import HelpPage from "./components/HelpPage";
 import NotFoundPage from "./components/NotFoundPage";
 
-function App() {
+const theme = createTheme();
+const useStyles = makeStyles({
+  root: {},
+  appBar: {
+    borderRadius: 15,
+    margin: "10px 0",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: "10px",
+    backgroundColor: "white",
+  },
+});
+
+const App = () => {
   const classes = useStyles();
   const title = "Expenses";
   return (
-    <Container>
-      <BrowserRouter>
-        <AppBar className={classes.appBar} position="static" color="inherit">
-          <Typography className={classes.heading} variant="h2">
-            {title}
-          </Typography>
-          <Navbar />
-        </AppBar>
-        <Switch>
-          <Route exact path="/" component={ExpenseDashboardPage} />
-          <Route path="/login" component={LoginPage} />
-          <Route path="/help" component={HelpPage} />
-          <Route component={NotFoundPage} />
-        </Switch>
-      </BrowserRouter>
-    </Container>
+    <ThemeProvider theme={theme}>
+      <Container>
+        <BrowserRouter>
+          <AppBar className={classes.appBar} position="static" color="inherit">
+            <Typography className={classes.heading} variant="h2">
+              {title}
+            </Typography>
+            <Navbar />
+          </AppBar>
+          <Switch>
+            <Route exact path="/" component={ExpenseDashboardPage} />
+            <Route path="/login" component={LoginPage} />
+            <Route path="/help" component={HelpPage} />
+            <Route component={NotFoundPage} />
+          </Switch>
+        </BrowserRouter>
+      </Container>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
